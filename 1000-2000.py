274c6251937b49e2a1c737eae7299e173b06b338
Your Name <you@example.com>
2021-05-21 19:54:16 +0000

    Initial commit

---
+++ 1000-2000.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by: Austin de Mora
+# Created on: May 2021
+# This program prints the numbers 1000 to 2000 with five integers per line
+
+
+def main():
+    # This function prints the numbers
+
+    # Process & Output
+    print("This program prints the integers from 1000 to 2000.")
+    print("")
+    for loop_counter in range(1000, 2000 + 1):
+        if loop_counter % 5 == 4:
+            print("{} ".format(loop_counter))
+        else:
+            print("{} ".format(loop_counter), end="")
+    print("\n\nDone")
+
+
+if __name__ == "__main__":
+    main()
